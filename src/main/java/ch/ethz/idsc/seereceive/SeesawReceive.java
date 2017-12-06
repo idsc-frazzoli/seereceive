c1774f4bef298037b23cfc3cfc60f6df151cb0f3
julian121266 <julianzilly@googlemail.com>
2017-12-06 21:57:46 +0100

    adding file to specify which port to listen to

--- src/main/java/ch/ethz/idsc/seereceive/SeesawReceive.java
+++ src/main/java/ch/ethz/idsc/seereceive/SeesawReceive.java
@@ -1,6 +1,12 @@
 // code by clruch
 package ch.ethz.idsc.seereceive;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import ch.ethz.idsc.seereceive.core.SeesawClient;
 import ch.ethz.idsc.seereceive.core.UartClientInterface;
 import ch.ethz.idsc.seereceive.core.UartServer;
@@ -8,11 +14,14 @@ import ch.ethz.idsc.seereceive.core.UartServer;
 /** the port has to be configured and may even change between two connects. */
 enum SeesawReceive {
   ;
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException, IOException {
     System.out.println("now reading from the seesaw...");
     // private final String PORT = "/dev/stlinkv2_console";
     // /dev/ttyACM1
-    UartClientInterface seesawclient = new SeesawClient("/dev/tty.usbmodem1413");
+    Properties properties = new Properties();
+    properties.load(new FileInputStream(new File("port.properties")));
+    
+    UartClientInterface seesawclient = new SeesawClient(properties.getProperty("port"));
     UartServer.create(seesawclient);
   }
 }
